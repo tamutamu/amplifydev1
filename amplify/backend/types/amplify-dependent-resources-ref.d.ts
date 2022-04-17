2962ab5bdde46184769d80bf4e71fa557bbe7f7e
tamutamu <tamu.0.0.tamu@gmail.com>
2022-04-17 10:47:03 +0900

    updat

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -8,12 +8,30 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "developerGroupRole": "string",
+            "adminGroupRole": "string"
         }
     },
     "api": {
         "amplifydev1": {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "amplifydev11489f209PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "graphQlLambdaAuthorizer4172ccb1": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
